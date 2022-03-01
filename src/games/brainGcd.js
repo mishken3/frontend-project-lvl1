a7c27848cdd350dd7dff7dbe50d79e25cf5da70d
Mikhail <mishken3@gmail.com>
2022-03-02 00:11:54 +0300

    add brain-gcd

---
+++ src/games/brainGcd.js
@@ -0,0 +1,26 @@
+import random from 'lodash/random.js';
+import { gcd } from 'mathjs';
+import playBrainGame, { roundsCount } from '../index.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const gcdLogic = () => {
+  const number1 = random(1, 100);
+  const number2 = random(1, 100);
+  // console.log('test');
+
+  const question = `${number1} ${number2}`;
+  const answer = gcd(number1, number2);
+
+  return [question, answer];
+};
+
+const brainGcd = () => {
+  const rounds = [];
+  for (let round = 0; round < roundsCount; round += 1) {
+    rounds.push(gcdLogic());
+  }
+  playBrainGame(gameDescription, rounds);
+};
+
+export default brainGcd;
