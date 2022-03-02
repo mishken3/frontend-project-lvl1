108b0b0c61804da0556c265f9da593e5e5481bde
Mikhail <mishken3@gmail.com>
2022-03-02 15:26:28 +0300

    remove npm mathjs; brain-gcd pass tests

--- src/games/brainGcd.js
+++ src/games/brainGcd.js
@@ -1,35 +1,16 @@
 import random from 'lodash/random.js';
-// import { gcd } from 'mathjs';
 import playBrainGame, { roundsCount } from '../index.js';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const getGCD = (num1, num2) => {
-  let temp = 0;
-  let first = num1;
-  let second = num2;
-  while (second > 0) {
-    temp = first % second;
-    first = second;
-    second = temp;
-  }
-  return first;
-};
+const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
 
 const gcdLogic = () => {
-  /*
-  const number1 = random(1, 100);
-  const number2 = random(1, 100);
-*/
   const firstNumber = random(1, 100);
   const secondNumber = random(1, 100);
 
   const question = `${firstNumber} ${secondNumber}`;
 
-  // console.log('question!?! ->', question);
-  // console.log('typeof question!?! ->', typeof question);
-
-  // const answer = String(gcd(firstNumber, secondNumber));
   const answer = String(getGCD(firstNumber, secondNumber));
 
   return [question, answer];
