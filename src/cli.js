e75afcf146563ff7041300aba1986b19fe4b0064
Mikhail <87711559+mishken3@users.noreply.github.com>
2023-10-04 23:40:18 +0300

    Feature/step three (#3)
    * installed package
    
    * greetings by name
    
    * update package version

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from "readline-sync";
+
+export const greetings = () => {
+  const name = readlineSync.question("May I have your name? ");
+
+  if (name) {
+    console.log(`Hello, ${name}!`);
+  }
+};
