9fde83d775fb5d92719c4ebe70ac14963f288d5f
Mikhail <mishken3@gmail.com>
2023-10-05 23:27:02 +0300

    added isEvenGame

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 export const greetings = () => {
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
 
   if (name) {
     console.log(`Hello, ${name}!`);
+
+    return name;
   }
 };
