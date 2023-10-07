000e90f8db7311c32d738c2b8d36c8c1a55d00d8
Mikhail <mishken3@gmail.com>
2023-10-07 14:00:15 +0300

    refactoring: moved logic to src

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,40 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { isEvenGame } from '../src/even-logic.js';
 import { greetings } from '../src/cli.js';
-
-const MAX_GAMES_COUNTER = 3;
-
-const EXPLANATION_GAME_TEXT =
-  'Answer "yes" if the number is even, otherwise answer "no".';
-
-const generateNumber = () => Math.floor(Math.random() * 100);
-const getIsEvenNumber = (number) => number % 2 === 0;
-
-const isEvenGameProcess = (userName, gameCounter) => {
-  let counter = gameCounter;
-  if (counter === MAX_GAMES_COUNTER) {
-    console.log(`Congratulations, ${userName}`);
-    return;
-  }
-
-  const randomNumber = generateNumber();
-  const isEvenNumber = getIsEvenNumber(randomNumber) ? 'yes' : 'no';
-  console.log(`Question: ${randomNumber}`);
-  const userInput = readlineSync.question('Your answer: ');
-
-  if (userInput === isEvenNumber) {
-    console.log('Correct!');
-    isEvenGameProcess(userName, counter + 1);
-  } else {
-    const correctionText = `'${userInput}' is wrong answer ;(. Correct answer was '${isEvenNumber}'.`;
-    console.log(correctionText);
-  }
-};
-
-const isEvenGame = (userName) => {
-  console.log(EXPLANATION_GAME_TEXT);
-
-  isEvenGameProcess(userName, 0);
-};
 
 isEvenGame(greetings());
--- src/cli.js
+++ src/cli.js
@@ -8,4 +8,6 @@ export const greetings = () => {
 
     return name;
   }
+
+  return null;
 };
